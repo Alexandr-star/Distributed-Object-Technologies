d547b556ae0ae143dfa80a3382ee45a72f0c84f3
Alexandr Skvortsov <sanya-bass.skvortzoff@yandex.ru>
2021-03-02 16:13:08 +0300

    create pretty view; fix view mac address

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,88 +1,59 @@
-import java.net.InetAddress;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Enumeration;
 
 public class Main {
 
-    public static void main(String[] args) throws SocketException, UnknownHostException {
-        InetAddress iAddress = null;
-        try
-        {
-            iAddress = InetAddress.getLocalHost();
-        }
-        catch(UnknownHostException ex)
-        {
-            System.out.println(ex.toString());
-        }
+    public static void main(String[] args) throws SocketException {
+        printLine();
+        System.out.println("Information about present Network Interfaces...");
+        printLine();
 
-        printInetAddresssInfo(iAddress);
+        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+        for (NetworkInterface networkInterface : Collections.list(nets))
+            displayInterfaceInformation(networkInterface);
+    }
 
-        // getNetworkInterfaces () возвращает список всех интерфейсов
-        // присутствует в системе.
-        ArrayList<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
-
-        System.out.println("\nInformation about present Network Interfaces...\n");
-
-        for (NetworkInterface networkInterface : interfaces) {
-            // находится ли интерфейс в процессе, запущен или работает или нет.
+    static void displayInterfaceInformation(NetworkInterface networkInterface) {
+        try {
             if (networkInterface.isUp()) {
                 System.out.println("Interface Name: " + networkInterface.getName());
 
                 System.out.println("Interface display name: " + networkInterface.getDisplayName());
-
-                System.out.println("Parent: " + networkInterface.getParent());
 
                 System.out.println("Index: " + networkInterface.getIndex());
 
                 System.out.println("MAC Address: " + getStringMacAddress(networkInterface.getHardwareAddress()));
 
                 // Интерфейсные адреса сетевого интерфейса
-                System.out.println("\tInterface addresses: ");
+                System.out.println("Interface addresses: ");
 
-                for (InterfaceAddress address : networkInterface.getInterfaceAddresses()) {
+                for (InterfaceAddress address : networkInterface.getInterfaceAddresses())
+                    System.out.println("\tIP Address:" + address.getAddress().toString());
 
-                }
-
-                System.out.println("\tInetAddresses associated with this interface: ");
-
-                Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
-
-                while (inetAddresses.hasMoreElements()) {
-
-                }
+            } else {
+                System.out.println("Interface " + networkInterface + " don't work.");
             }
+        } catch (SocketException e) {
+            System.out.println("Warning: " + e.toString());
         }
+
+        printLine();
     }
 
     private static String getStringMacAddress(byte[] hardwareAddress) {
-        if (hardwareAddress == null) return "none";
+        if (hardwareAddress == null) return "Address doesn't exist or is not accessible.";
 
-        String macAddress = "";
-        for (byte bAddress: hardwareAddress) {
-            macAddress += bAddress + ":";
-        }
+        StringBuilder macAddress = new StringBuilder();
+        for (int indexByte = 0; indexByte < hardwareAddress.length; indexByte++)
+            macAddress.append(String.format("%02X%s", hardwareAddress[indexByte], indexByte == hardwareAddress.length - 1 ? "" : ":"));
 
-        return macAddress;
+        return macAddress.toString();
     }
 
-    private static void printInetAddresssInfo(InetAddress iaRemoteAddress) {
-        System.out.println("Address: " + iaRemoteAddress);
-
-        System.out.println("Host name: " + iaRemoteAddress.getHostName());
-
-        byte[] ip = new byte[4];
-        ip = iaRemoteAddress.getAddress();
-
-        System.out.println("IP Address: " +
-                (0xff & (int)ip[0]) + "." +
-                (0xff & (int)ip[1]) + "." +
-                (0xff & (int)ip[2]) + "." +
-                (0xff & (int)ip[3]));
+    private static void printLine() {
+        System.out.println("-----------------------------------------------------");
     }
 }
